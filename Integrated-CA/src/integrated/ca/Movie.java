66df4bb54d6fb0b57843e914f9a3a5d86a9b8321
Rayenbt11 <100359918+Rayenbt11@users.noreply.github.com>
2023-12-07 00:11:09 +0000

    create movie class

---
+++ Integrated-CA/src/integrated/ca/Movie.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package integrated.ca;
+
+/**
+ *
+ * @author rayen
+ */
+public class Movie {
+
+    
+    private String title;
+    private double price;
+
+    // Constructor
+    public Movie( String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{"
+                + ", title='" + title + '\''
+                + ", price=" + price
+                + '}';
+    }
+}
